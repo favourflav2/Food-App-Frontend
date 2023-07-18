0ff41693e3ef6287a903c46fb7e725212809d658
favourflav <chukwagon1@gmail.com>
2023-07-17 23:08:40 -0700

    added code splitttng in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Location from "./components/navbar/Location";
-import DirectionModal from "./components/models/DirectionModal";
-import Home from "./pages/Home/Home";
+//import DirectionModal from "./components/models/DirectionModal";
+//import Home from "./pages/Home/Home";
 import SuccessPage from "./pages/Stripe/Success";
 import ErrorPage from "./pages/Stripe/Error";
+
+const Home = lazy(() => import("./pages/Home/Home"))
+const DirectionModal = lazy(() => import("./components/models/DirectionModal"))
 
 function App() {
   // Open State for googel maps
@@ -21,22 +24,36 @@ function App() {
         <NavBar />
         <Location handleOpen={handleOpen} />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={
+            <Suspense fallback={<div className="homebg w-full h-screen flex items-center justify-center"><div>Loading...</div></div>}>
+              <Home />
+            </Suspense>
+          } />
 
           <Route
             path="/modal"
             element={
-              <DirectionModal
+              <Suspense fallback={<div className="sm:absolute sm:translate-x-[-50%] sm:translate-y-[-50%] sm:top-[50%] sm:left-[50%] sm:w-[480px] bg-white sm:h-auto h-full"><div>Loading...</div></div>}>
+                <DirectionModal
                 open={open}
                 handleOpen={handleOpen}
                 handleClose={handleClose}
                 setOpen={setOpen}
               />
+              </Suspense>
             }
           />
 
-          <Route path="/success" element={<SuccessPage />} />
-          <Route path="/error" element={<ErrorPage />} />
+          <Route path="/success" element={
+            <Suspense fallback={<div className="homebg w-full h-screen flex items-center justify-center"><div>Loading...</div></div>}>
+              <SuccessPage />
+            </Suspense>
+          } />
+          <Route path="/error" element={
+            <Suspense fallback={<div className="homebg w-full h-screen flex items-center justify-center"><div>Loading...</div></div>}>
+              <ErrorPage />
+            </Suspense>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
